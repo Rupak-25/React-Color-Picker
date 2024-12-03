7e6e4d922876d8d2c129863c21199455f48b5f3a
Rupak-25 <crupak64@gmail.com>
2024-12-03 15:29:58 +0530

    Color Picker App

---
+++ reactcolorpickerapp/src/App.jsx
@@ -0,0 +1,10 @@
+
+import Colorpicker from './Colorpicker'
+
+const App = () => {
+  return (
+    <Colorpicker/>
+  )
+}
+
+export default App
---
+++ reactcolorpickerapp/src/Colorpicker.jsx
@@ -0,0 +1,27 @@
+import {useState} from 'react';
+
+
+function Colorpicker (){
+    const [color, setColor] = useState('#FFFFFF');
+
+    function handleColorChange(event){
+        setColor(event.target.value);
+    }
+    
+    return (
+    <>
+      <div className="color-picker">
+        <h1>Color Picker</h1>
+        <div className="color-display" style={{backgroundColor: color}}>
+           <p>Selected Color: {color}</p>
+        </div>
+        <label>Select a Color:</label>
+        <input type="color" value={color} onChange={handleColorChange}/>
+      </div>
+    </>
+  );
+}
+
+export default Colorpicker;
+
+
